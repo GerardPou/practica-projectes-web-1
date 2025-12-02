0088447abeb2c6134927a3eaa3bb98844accc7d0
gerard.pou <gerard.pou@students.salle.url.edu>
2025-12-02 17:41:13 +0100

    buscador de weather

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const API_KEY = 'e37da9bf540393b942e08990dc919de9';
+const CITY_NAME_SEARCH = 'Barcelona';
+const API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${CITY_NAME_SEARCH}&limit=5&appid=${API_KEY}`;
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const navButtons = document.querySelectorAll('[data-page]');
 
@@ -11,3 +16,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
