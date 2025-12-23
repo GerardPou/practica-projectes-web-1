91eb9b540f61bb3656022b855b65bf85ec7789a9
Javier Fernàndez Vidal-Quadras <javier.fv@students.salle.url.edu>
2025-12-23 21:35:08 +0100

    Logica de Añadir Viaje hecho

--- scripts/trips.js
+++ scripts/trips.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const botonAbrir = document.querySelector('#Button_CrearViaje');
     const botonCerrar = document.querySelector('#btn-cancelar');
     const botonX = document.querySelector('#btn-x-cerrar');
-
-    const elementoHTML = document.getElementById('miModalBootstrap');
-    const miModal = new bootstrap.Modal(elementoHTML);
+    const botonGuardar = document.getElementById('btn-guardar');
+    let misViajes = [];
+    const elementoModal = document.getElementById('miModalBootstrap');
+    const miModal = new bootstrap.Modal(elementoModal);
     botonAbrir.addEventListener('click', () => {
         miModal.show();
     });
@@ -14,4 +15,66 @@ document.addEventListener('DOMContentLoaded', () => {
     botonX.addEventListener('click', () => {
         miModal.hide();
     });
+    botonGuardar.addEventListener('click', () => {
+        guardarViajeForm(miModal);
+    })
+
+    const ViajesGuardados=localStorage.getItem('misViajes');
+    if (ViajesGuardados != null) {
+        misViajes=JSON.parse(ViajesGuardados);
+        printarViajes();
+    }
+
+    function guardarViajeForm(modalBootstrap){
+        const Inputpais= document.getElementById('LabelPais');
+        const Inputciudad=document.getElementById('LabelCiudad');
+        const InputfechaIda = document.getElementById('LabelFechaIda');
+        const InputfechaFin = document.getElementById('LabelFechaVuelta');
+
+        const pais = Inputpais.value;
+        const ciudad = Inputciudad.value;
+        const fechaIda = InputfechaIda.value;
+        const fechaFin = InputfechaFin.value;
+
+        if (pais === "" || ciudad === ""){
+            alert("Falta rellenar el pais o la ciudad");
+            return;
+        }
+        if (fechaFin === "" || fechaIda === ""){
+            alert("Falta rellenar las fechas");
+            return;
+        }
+
+        const nuevoViaje= {
+            fechaInicio: fechaIda,
+            fechaFin: fechaFin,
+            ciudad: ciudad,
+            pais: pais,
+        }
+        misViajes.push(nuevoViaje);
+        localStorage.setItem('misViajes', JSON.stringify(misViajes));
+        printarViajes();
+        document.getElementById('formCrear').reset();
+        modalBootstrap.hide();
+    }
+    function printarViajes(){
+        const contenedorViajes=document.getElementById('contenedor-viajes');
+        let num_viajes=misViajes.length;
+        contenedorViajes.innerHTML = "";
+        for (i=0;i<num_viajes;i++){
+            let viaje=misViajes[i];
+            contenedorViajes.innerHTML +=`
+                <div class="card" >
+                <div class="card-body">
+                    <h5 class="card-title">${viaje.ciudad}, ${viaje.pais}</h5>
+                 <p class="card-text text-muted">
+                         <strong>Ida:</strong> ${viaje.fechaInicio} <br>
+                         <strong>Vuelta:</strong> ${viaje.fechaFin}
+                    </p>
+                </div>
+            </div>`;
+
+
+        }
+    }
 });
